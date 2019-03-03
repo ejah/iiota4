458fb4635abdc022eb59cb0ae5b90e76efc832ae
Ernst-Jan Huijbers <info@iiota.nl>
2019-03-03 20:20:42 +0100

    Model CarouselItem uitgebreid met index field

--- djangocms_owl_carousel/models.py
+++ djangocms_owl_carousel/models.py
@@ -8,6 +8,7 @@ class CarouselHolder(CMSPlugin):
 
 class CarouselItem(CMSPlugin):
     image_thumb = models.ImageField(verbose_name='Thumb image', null=True)
+    slide_index = models.IntegerField(verbose_name='slide_index', null=False, default=1)
 
     class Meta:
         verbose_name_plural = "Carousel Items"

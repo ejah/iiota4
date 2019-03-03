8dcac7d480b2a7084e759fc95fa91dfc31a24a17
Ernst-Jan Huijbers <info@iiota.nl>
2019-03-04 00:26:09 +0100

    Model CarouselItem uitgebreid met text en caption, en uitgebreid met children plugins ooptie voor UrlLink.

--- djangocms_owl_carousel/cms_plugins.py
+++ djangocms_owl_carousel/cms_plugins.py
@@ -1,6 +1,7 @@
 from cms.plugin_base import CMSPluginBase
 from cms.plugin_pool import plugin_pool
 from cms.models.pluginmodel import CMSPlugin
+from djangocms_link.cms_plugins import LinkPlugin
 from django.utils.translation import ugettext_lazy as _
 
 from djangocms_owl_carousel.models import CarouselHolder, CarouselItem
@@ -11,6 +12,11 @@ class CarouselHolderPlugin(CMSPluginBase):
     name = _("Carousel Holder")
     render_template = 'carousel_holder.html'
     cache = False
+    allow_children = True
+
+    def render(self, context, instance, placeholder):
+        context = super(CarouselHolderPlugin, self).render(context, instance, placeholder)
+        return context
 
 @plugin_pool.register_plugin
 class CarouselItemPlugin(CMSPluginBase):
@@ -18,6 +24,9 @@ class CarouselItemPlugin(CMSPluginBase):
     name = _("Carousel Item")
     render_template = 'carousel_item.html'
     cache = False
+    require_parent = True
+    allow_children = True
+    child_classes = ['LinkPlugin',]
 
     def render(self, context, instance, placeholder):
         context = super(CarouselItemPlugin, self).render(context, instance, placeholder)
--- djangocms_owl_carousel/models.py
+++ djangocms_owl_carousel/models.py
@@ -9,6 +9,8 @@ class CarouselHolder(CMSPlugin):
 class CarouselItem(CMSPlugin):
     image_thumb = models.ImageField(verbose_name='Thumb image', null=True)
     slide_index = models.IntegerField(verbose_name='slide_index', null=False, default=1)
+    caption = models.CharField(max_length=100, null=True)
+    text = models.TextField(max_length=500, null=True)
 
     class Meta:
         verbose_name_plural = "Carousel Items"
